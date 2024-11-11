fb75a545a6df4372185c2261c8bc5652c79d756c
asadbekumarov <asadbekumarov922@gmail.com>
2024-11-12 00:31:34 +0500

    end

---
+++ script.js
@@ -0,0 +1,30 @@
+const Elbtn = document.querySelector("button");
+const Elres = document.querySelector(".result");
+const Elinput = document.querySelector("input");
+let magic = [
+  "It is certain.",
+  "Ask again later.",
+  "Don't count on it.",
+  "Yes, definitely!",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Signs point to yes.",
+  "Better not tell you now.",
+];
+
+function random() {
+  return magic[Math.floor(Math.random() * magic.length)];
+}
+
+Elbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (Elinput.value === "") {
+    alert("Savolingizni kirting");
+  } else {
+    Elres.textContent = random();
+    Elinput.value = "";
+    setTimeout(() => {
+      Elres.textContent = "";
+    }, 3500);
+  }
+});
